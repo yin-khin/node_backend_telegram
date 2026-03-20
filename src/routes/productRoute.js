8cf87d9f1cb699ba5c273c4d9b7535635644b117
prengkhmer <prengkhmer1999@gmail.com>
2026-03-20 23:26:14 +0700

    node api telegram

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -5,13 +5,13 @@ var {
   updateProduct,
   deleteProduct,
 } = require("../controllers/productController");
-const auth = require("../middlewares/auth.middleware");
+// const auth = require("../middlewares/auth.middleware");
 const product = (app) => {
-  app.get("/api/products", auth.validate_token(),getAllProduct);
-  app.get("/api/products/:id", auth.validate_token(),getProductById);
-  app.post("/api/products", auth.validate_token(),createProduct);
-  app.put("/api/products/:id", auth.validate_token(),updateProduct);
-  app.delete("/api/products/:id", auth.validate_token(),deleteProduct);
+  app.get("/api/products",getAllProduct);
+  app.get("/api/products/:id", getProductById);
+  app.post("/api/products", createProduct);
+  app.put("/api/products/:id", updateProduct);
+  app.delete("/api/products/:id", deleteProduct);
 };
 
 module.exports = product;
